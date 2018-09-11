87fc3c6cca84a5f61dfb23c792988db880d59967
Faishal Alif Fadhillah <emailkerjaisal@gmail.com>
2018-09-11 13:02:03 +0700

    off

---
+++ OffWhite/MODEL/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  OffWhite
+//
+//  Created by Faishal Alif on 8/8/18.
+//  Copyright © 2018 Faishal Alif. All rights reserved.
+//
+
+import Foundation
---
+++ OffWhite/MODEL/Product.swift
@@ -0,0 +1,31 @@
+//
+//  Product.swift
+//  OffWhite
+//
+//  Created by Faishal Alif on 8/8/18.
+//  Copyright © 2018 Faishal Alif. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+// Represents a generic product. Need an image named "default"
+
+class Product
+{
+    var title: String
+    var description: String
+    
+   
+    
+    init(titled: String, description: String)
+    {
+        self.title = titled
+        self.description = description
+       
+        
+        
+}
+}
